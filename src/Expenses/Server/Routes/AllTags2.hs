e74f8ae1015dc3d78282b663f53d1e701740ba68
Diogo Castro <dc@diogocastro.com>
2026-05-19 22:35:16 +0100

    Cloned `Routes` module

---
+++ src/Expenses/Server/Routes/AllTags2.hs
@@ -0,0 +1,15 @@
+module Expenses.Server.Routes.AllTags2 where
+
+import Config qualified
+import CustomPrelude
+import Data.Set qualified as Set
+import Database qualified as Db
+import Expenses.Server.AppM (AppM, Env (..), useConnection)
+import Types
+
+allTagsHandler :: AppM (Set TagName)
+allTagsHandler = do
+  dbTags <- useConnection \conn -> liftIO $ Db.getAllTags conn
+  appConfig <- asks (.config)
+  let allTags = Set.fromList dbTags `Set.union` Config.allKnownTags appConfig
+  pure allTags
